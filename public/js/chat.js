9ae29ab92e02c3c64c6907afd1a4e8dc94ca618a
Owen <owenmccr@gmail.com>
2023-11-28 23:12:59 -0500

    2 very basic chatrooms implemented

---
+++ public/js/chat.js
@@ -0,0 +1,88 @@
+const socket = io();
+const app = document.querySelector(".app");
+
+document.querySelectorAll(".chatrooms").forEach((chatroom) => {
+  chatroom.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (
+      window.location.href.charAt(window.location.href.length - 1) ===
+      chatroom.id
+    ) {
+      socket.emit("join", "New Room");
+      return;
+    } else {
+      document.location.replace(chatroom.href);
+    }
+  });
+});
+
+let uname = "username";
+socket.emit("newuser", uname);
+
+app
+  .querySelector(".chat-screen #send-message")
+  .addEventListener("click", function () {
+    let message = app.querySelector(".chat-screen #message-input").value;
+    if (message.length == 0) {
+      return;
+    }
+
+    renderMessage("my", {
+      username: uname,
+      text: message,
+    });
+
+    socket.emit("chat", {
+      username: uname,
+      text: message,
+    });
+
+    app.querySelector(".chat-screen #message-input").value = "";
+  });
+
+app
+  .querySelector(".chat-screen #exit-chat")
+  .addEventListener("click", function () {
+    socket.emit("exituser", uname);
+    window.location.href = window.location.href;
+  });
+
+socket.on("update", function (update) {
+  renderMessage("update", update);
+});
+
+socket.on("chat", function (message) {
+  renderMessage("other", message);
+});
+
+function renderMessage(type, message) {
+  let messageContainer = app.querySelector(".chat-screen .messages");
+  if (type == "my") {
+    let el = document.createElement("div");
+    el.setAttribute("class", "message my-message");
+    el.innerHTML = `
+          <div>
+            <div class="name">You</div>
+            <div class="text">${message.text}</div>
+        `;
+    messageContainer.appendChild(el);
+  } else if (type == "other") {
+    let el = document.createElement("div");
+    el.setAttribute("class", "message other-message");
+    el.innerHTML = `
+          <div>
+            <div class="name">${message.username}</div>
+            <div class="text">${message.text}</div>
+          </div>
+        `;
+    messageContainer.appendChild(el);
+  } else if (type == "update") {
+    let el = document.createElement("div");
+    el.setAttribute("class", "update");
+    el.innerText = message;
+    messageContainer.appendChild(el);
+  }
+  // scroll chat to end on new message
+  messageContainer.scrollTop =
+    messageContainer.scrollHeight - messageContainer.clientHeight;
+}
--- server.js
+++ server.js
@@ -1,44 +1,81 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 // const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
+const server = createServer(app);
 const PORT = process.env.PORT || 3001;
+
+const io = new Server(server);
 
 const hbs = exphbs.create();
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {
     maxAge: 300000,
     httpOnly: true,
     secure: false,
-    sameSite: 'strict',
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // socket.emit("rooms", rooms);
+
+  socket.on("join", function (room) {
+    socket.join(room);
+    socket.room = room;
+    console.log("User connected to room:", socket.room);
+  });
+
+  socket.on("chat", function (message) {
+    io.to(socket.room).emit("chat", message);
+
+    console.log(message);
+  });
+});
+
+// io.on("connection", function (socket) {
+//   socket.on("newuser", function (username) {
+//     socket.broadcast.emit("update", username + " joined the conversation");
+//   });
+//   socket.on("exituser", function (username) {
+//     socket.broadcast.emit("update", username + " left the conversation");
+//   });
+//   socket.on("chat", function (message) {
+//     socket.broadcast.emit("chat", message);
+//   });
+// });
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening at http://localhost:' + PORT));
+  server.listen(PORT, () =>
+    console.log("Now listening at http://localhost:" + PORT)
+  );
 });
