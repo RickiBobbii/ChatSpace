fef25b32cef098f876dece5c418b95dd4de6d723
Owen <owenmccr@gmail.com>
2023-11-28 20:39:02 -0500

    testing page and adding chatroom section

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,29 @@
 const router = require("express").Router();
+const Chatroom = require("../models/Chatroom");
 
-router.get("/", (req, res) => {
-  if (!req.session.logged_in) {
-    res.redirect("/login");
-    return;
+router.get("/", async (req, res) => {
+  res.redirect("/login");
+});
+
+router.get("/testing", async (req, res) => {
+  // if (!req.session.logged_in) {
+  //   res.redirect("/login");
+  //   return;
+  // }
+  try {
+    const chatroomData = await Chatroom.findAll({});
+
+    const chatrooms = chatroomData.map((chatroom) =>
+      chatroom.get({ plain: true })
+    );
+
+    res.render("testing", {
+      chatrooms: chatrooms,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.render("homepage");
 });
 
 router.get("/login", (req, res) => {
