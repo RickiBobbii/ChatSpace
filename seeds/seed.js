1d3cd9afb5197ad3dd31d2ad57ab6220dace68b4
Owen <owenmccr@gmail.com>
2023-12-01 14:00:59 -0500

    user model and some seed data for testing

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,18 @@
 const sequelize = require("../config/connection");
-const { Chatroom } = require("../models");
+const { Chatroom, User } = require("../models");
 
 const chatroomData = require("./chatroomData.json");
+const userData = require("./userData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await Chatroom.bulkCreate(chatroomData, {
+  const chatrooms = await Chatroom.bulkCreate(chatroomData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
