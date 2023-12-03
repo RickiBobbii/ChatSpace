657a7b365c5bcd7c09f3b7e635f621620e248652
RickiBobbii <rickibobbiii@gmail.com>
2023-12-02 19:39:57 -0500

    deleted previous unused models

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,7 @@
 const router = require('express').Router();
-const animalRoutes = require('./animalRoute');
-const movieRoutes = require('./movieRoute');
-const musicRoutes = require('./musicRoute');
-const projectRoutes = require('./projectRoute');
 const userRoutes = require('./userRoutes');
 
 //Add user route
 router.use('/users', userRoutes);
-router.use('/animals', animalRoutes);
-router.use('/movies', movieRoutes);
-router.use('/music', musicRoutes);
-router.use('/projects', projectRoutes);
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,8 @@
 const sequelize = require("../config/connection");
-const { Chatroom, User, Animal, Movie, Music, Project } = require("../models");
+const { Chatroom, User } = require("../models");
 
 const chatroomData = require("./chatroomData.json");
 const userData = require("./userData.json");
-const animalData = require('./animalData.json');
-const movieData = require('./movieData.json');
-const musicData = require('./musicData.json');
-const projectData = require('./projectData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -20,30 +16,6 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-
-  for (const animal of animalData) {
-    await Animal.create({
-      ...animal,
-    });
-  };
-
-  for (const movie of movieData) {
-    await Movie.create({
-      ...movie,
-    });
-  };
-
-  for (const music of musicData) {
-    await Music.create({
-      ...music,
-    });
-  };
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-    });
-  };
 
   process.exit(0);
 };
